6959c49db43cc2b011400b8d73d24f447b8d3dc4
Walter Gysin <walter@botmaker.com>
2022-08-29 20:08:09 -0300

    padding back

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,6 +22,7 @@ const Home = () => {
       <div
         className="no-scroll"
         style={{
+          padding: '1rem',
           overflow: 'scroll',
           display: 'flex',
           flexWrap: 'wrap',
